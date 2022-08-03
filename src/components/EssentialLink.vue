ff4c82d2af44f2eebea38a4956ddcdc703127f9e
danielolabemiwo <olabemiwodaniel@gmail.com>
2022-08-03 20:47:55 +0100

    [Feat]: Header page setup

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    :href="link"
+    class="links"
+    :class="{ isActive: isSelect }"
+  >
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label style="margin-left: -22px">{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "EssentialLink",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    link: {
+      type: String,
+      default: "#",
+    },
+
+    icon: {
+      type: String,
+      default: "",
+    },
+    isSelect: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style>
+.links {
+  font-weight: 900;
+  color: #45484a;
+}
+.isActive {
+  border-bottom: 3px #4d506b solid;
+  color: #362ea1;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex q-px-lg">
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    /> -->
+    <q-breadcrumbs>
+      <q-breadcrumbs-el label="Invoices" />
+      <q-breadcrumbs-el label="edit invoice (INV-2022-010)" />
+    </q-breadcrumbs>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
